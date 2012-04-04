42ecea550e42d756a351a80f801a74d9fe393ab1
Tom Wright <tom@tdwright.co.uk>
2012-04-04 18:52:53 +0100

    Video capture works

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -28,20 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(292, 266);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(292, 266);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Main";
             this.Text = "Creole SSD";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Main.cs
+++ Main.cs
@@ -6,14 +6,46 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using AForge.Video.DirectShow;
+using AForge.Video;
 
 namespace CreoleSSD
 {
     public partial class Main : Form
     {
+
+        VideoCaptureDevice videoSource;
+
         public Main()
         {
             InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            // enumerate video devices
+            FilterInfoCollection videoDevices = new FilterInfoCollection(
+                    FilterCategory.VideoInputDevice);
+            // create video source
+            this.videoSource = new VideoCaptureDevice(
+                    videoDevices[0].MonikerString);
+            // set frame rate
+            this.videoSource.DesiredFrameRate = 10;
+            // set NewFrame event handler
+            this.videoSource.NewFrame += new NewFrameEventHandler(video_NewFrame);
+            // start the video source
+            this.videoSource.Start();
+        }
+
+        private void video_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Image tmp = (Image)eventArgs.Frame.Clone();
+            this.pictureBox1.Image = tmp;
+        }
+
+        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.videoSource.SignalToStop();
         }
     }
 }
