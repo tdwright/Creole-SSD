de10de298f787ebc75239827207ad6531ed2ac79
Tom Wright <tom@tdwright.co.uk>
2012-04-11 18:52:17 +0100

    Gets values in 32x32 ROI (fixed as centre) and weights as 4x5 foveal map

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -28,38 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.rawPictureBox = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.repPictureBox = new System.Windows.Forms.PictureBox();
+            this.ROIbox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.rawPictureBox)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.repPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ROIbox)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // rawPictureBox
             // 
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(292, 266);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.rawPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rawPictureBox.Location = new System.Drawing.Point(3, 3);
+            this.rawPictureBox.Name = "rawPictureBox";
+            this.rawPictureBox.Size = new System.Drawing.Size(229, 175);
+            this.rawPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.rawPictureBox.TabIndex = 0;
+            this.rawPictureBox.TabStop = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Controls.Add(this.ROIbox, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.rawPictureBox, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.repPictureBox, 2, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(708, 181);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // repPictureBox
+            // 
+            this.repPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.repPictureBox.Location = new System.Drawing.Point(473, 3);
+            this.repPictureBox.Name = "repPictureBox";
+            this.repPictureBox.Size = new System.Drawing.Size(232, 175);
+            this.repPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.repPictureBox.TabIndex = 2;
+            this.repPictureBox.TabStop = false;
+            // 
+            // ROIbox
+            // 
+            this.ROIbox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ROIbox.Location = new System.Drawing.Point(238, 3);
+            this.ROIbox.Name = "ROIbox";
+            this.ROIbox.Size = new System.Drawing.Size(229, 175);
+            this.ROIbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ROIbox.TabIndex = 3;
+            this.ROIbox.TabStop = false;
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 266);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(708, 181);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Main";
             this.Text = "Creole SSD";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
             this.Load += new System.EventHandler(this.Main_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rawPictureBox)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.repPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ROIbox)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox rawPictureBox;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox ROIbox;
+        private System.Windows.Forms.PictureBox repPictureBox;
     }
 }
 
--- Main.cs
+++ Main.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows.Forms;
 using AForge.Video.DirectShow;
@@ -15,6 +17,7 @@ namespace CreoleSSD
     {
 
         VideoCaptureDevice videoSource;
+        Rectangle ROI;
 
         public Main()
         {
@@ -23,6 +26,8 @@ namespace CreoleSSD
 
         private void Main_Load(object sender, EventArgs e)
         {
+            // initialise ROI
+            this.ROI = new Rectangle(304, 224, 32, 32);
             // enumerate video devices
             FilterInfoCollection videoDevices = new FilterInfoCollection(
                     FilterCategory.VideoInputDevice);
@@ -39,8 +44,125 @@ namespace CreoleSSD
 
         private void video_NewFrame(object sender, NewFrameEventArgs eventArgs)
         {
-            Image tmp = (Image)eventArgs.Frame.Clone();
-            this.pictureBox1.Image = tmp;
+            int[,] rawValues = new int[32,32];
+            Bitmap tmp = (Bitmap)eventArgs.Frame.Clone();
+            
+            // throw away everything except the ROI
+            Bitmap sub = new Bitmap(this.ROI.Width, this.ROI.Height);
+            using (Graphics g = Graphics.FromImage(sub))
+            {
+                g.DrawImage(tmp, 0, 0, this.ROI, GraphicsUnit.Pixel);
+            }
+
+            // Read out the values
+            BitmapData bmpData = sub.LockBits(new Rectangle(0, 0, sub.Width, sub.Height), ImageLockMode.ReadOnly, sub.PixelFormat);
+            IntPtr ptr = bmpData.Scan0;
+            int bytes = Math.Abs(bmpData.Stride) * sub.Height;
+            byte[] rgbValues = new byte[bytes];
+            Marshal.Copy(ptr, rgbValues, 0, bytes);
+            for (int i = 0; i < 1024; i++)
+            {
+                int row = (int)Math.Floor((double)i / 32d);
+                int col = i % 32;
+                int[] rgb = { rgbValues[i * 3], rgbValues[i * 3 + 1], rgbValues[i * 3 + 2] };
+                rawValues[col, row] = (int)Math.Round(rgb.Average());
+            }
+            sub.UnlockBits(bmpData);
+            
+            // Collapse the data
+            int[,] weightedValues = new int[4, 5];
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    int cumulative = 0;
+                    int radius = (int)Math.Pow(2, j);
+                    int excRadius = (int)Math.Pow(2, j-1);
+                    if (j==0) excRadius = 0;
+                    int incX; int excX;
+                    if((i%2)==0)
+                    {
+                        incX = 16 - radius;
+                        excX = (16 - radius) + excRadius;
+                    }
+                    else
+                    {
+                        incX = 16;
+                        excX = 16;
+                    }
+                    int incY; int excY;
+                    if(i<2)
+                    {
+                        incY = 16 - radius;
+                        excY = (16 - radius) + excRadius;
+                    }
+                    else
+                    {
+                        incY = 16;
+                        excY = 16;
+                    }
+                    Rectangle exclude = new Rectangle(excX, excY, excRadius, excRadius);
+                    for (int x = 0; x < radius; x++)
+                    {
+                        for (int y = 0; y < radius; y++)
+                        {
+                            int absX = incX+x;
+                            int absY = incY+y;
+                            if(!exclude.Contains(new Point(absX,absY)))
+                            {
+                                cumulative += rawValues[absX,absY];
+                            }
+                        }
+                    }
+                    int area = (int)(Math.Pow(radius, 2) - Math.Pow(excRadius, 2));
+                    weightedValues[i,j] = (int)Math.Round((double)cumulative/(double)area);
+                }
+            }
+
+            // Generate visual representation
+            int scalingfactor = 20;
+            Bitmap repSmall = new Bitmap(10 * scalingfactor, 10 * scalingfactor);
+            using (Graphics repG = Graphics.FromImage(repSmall))
+            {
+                int Xo, Yo;
+                for (int i = 0; i < 4; i++)
+                {
+                    if ((i % 2) == 0) Xo = -1; // Left half
+                    else Xo = 0; // Right half
+                    if (i < 2) Yo = -1; // Top half
+                    else Yo = 0; // Bottom half
+                    for (int j = 4; j >= 0; j--)
+                    {
+                        int radius = j + 1;
+                        Brush b = new SolidBrush(Color.FromArgb(weightedValues[i,j],weightedValues[i,j],weightedValues[i,j]));
+                        Rectangle r = new Rectangle((5 + (radius * Xo)) * scalingfactor, (5 + (radius * Yo)) * scalingfactor, radius * scalingfactor, radius * scalingfactor);
+                        repG.FillRectangle(b, r);
+                    }
+                }
+            }
+
+            // Draw ROI on original
+            using (Graphics g = Graphics.FromImage(tmp))
+            {
+                g.DrawRectangle(new Pen(new SolidBrush(Color.Red)), this.ROI);
+            }
+
+            // Update UI (via the correct thread)
+            this.rawPictureBox.BeginInvoke(new Action(
+                () =>
+                {
+                    this.rawPictureBox.Image = tmp;
+                }));
+            this.repPictureBox.BeginInvoke(new Action(
+                () =>
+                {
+                    this.repPictureBox.Image = repSmall;
+                }));
+            this.ROIbox.BeginInvoke(new Action(
+                () =>
+                {
+                    this.ROIbox.Image = sub;
+                }));
         }
 
         private void Main_FormClosing(object sender, FormClosingEventArgs e)
